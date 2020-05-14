f658e9d6aeca1d29342201226e8ac862b7de66b1
shashank2704 <47267540+shashank2704@users.noreply.github.com>
2020-05-14 23:38:35 +0530

    Dice Roller commit

--- DiceRoller2/app/src/main/java/diceroller/example/com/diceroller/MainActivity.kt
+++ DiceRoller2/app/src/main/java/diceroller/example/com/diceroller/MainActivity.kt
@@ -10,14 +10,16 @@ import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var diceImage: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-         val rollButton: Button=findViewById(R.id.roll_button)
+        val rollButton: Button=findViewById(R.id.roll_button)
         rollButton.text="Lets Roll the dice"
         rollButton.setOnClickListener{
             rollDice()
         }
+        diceImage=findViewById(R.id.dice_image)
     }
 
     private fun rollDice() {
@@ -31,7 +33,6 @@ class MainActivity : AppCompatActivity() {
             5 -> R.drawable.dice_5
             else -> R.drawable.dice_6
         }
-        val diceImage: ImageView=findViewById(R.id.dice_image)
         diceImage.setImageResource(drawableResource)
     }
 }
